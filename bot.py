5a970afa32f91855cfe16b54234ca0df630aa533
kulga04 <113110175+kulga04@users.noreply.github.com>
2022-12-19 10:31:49 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,47 @@
+import telebot
+from telebot import types  # для указание типов
+import config
+
+bot = telebot.TeleBot('5864614295:AAHyTtK4DumERRe7eB89n-HBXFbeY3DekjI')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton("❓ Задать вопрос")
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id,
+                     text="Привет, {0.first_name}! Я бот созданный для проекта чтоб получить хорошую оценку".format(
+                         message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if (message.text == "👋 Поздороваться"):
+        bot.send_message(message.chat.id, text="Привеет.. Спасибо что уделяешь мне внимание!)")
+    elif (message.text == "❓ Задать вопрос"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Как меня зовут?")
+        btn2 = types.KeyboardButton("Что я могу?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+    elif (message.text == "Как меня зовут?"):
+        bot.send_message(message.chat.id, "Меня зовут призывник,я скоро поеду в армию..")
+
+    elif message.text == "Что я могу?":
+        bot.send_message(message.chat.id, text="Поздороваться с читателями,ну и маршировать ")
+
+    elif (message.text == "Вернуться в главное меню"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("👋 Поздороваться")
+        button2 = types.KeyboardButton("❓ Задать вопрос")
+        markup.add(button1, button2)
+        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, text="На такую комманду я не запрограммировал..")
+
+
+bot.polling(none_stop=True)
